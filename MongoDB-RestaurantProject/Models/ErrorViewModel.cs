d6a79ae1174a5873cc4ae8af1b71b26c4d1eff4c
tunadeveloper <tunabusiness25@gmail.com>
2026-02-01 13:34:23 +0300

    Add project files.

---
+++ MongoDB-RestaurantProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MongoDB_RestaurantProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
