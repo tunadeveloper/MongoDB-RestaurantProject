980d65d447fe34690dcd27b9ff8edd2651ecba90
tunadeveloper <tunabusiness25@gmail.com>
2026-02-07 15:52:51 +0300

    Updated

---
+++ MongoDB-RestaurantProject/Areas/Admin/Controllers/NewsletterController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB_RestaurantProject.Context.Entities;
+using MongoDB_RestaurantProject.DataTransferObject.NewsletterDTOs;
+using MongoDB_RestaurantProject.Services.NewsletterService;
+using System.Threading.Tasks;
+
+namespace MongoDB_RestaurantProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class NewsletterController : Controller
+    {
+        private readonly INewsletterSerivce _newsletterService;
+        private readonly IMapper _mapper;
+        public NewsletterController(INewsletterSerivce newsletterService, IMapper mapper)
+        {
+            _newsletterService = newsletterService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var list = await _newsletterService.GetListAsync();
+            var result = _mapper.Map<List<ResultNewsletterDTO>>(list);
+            return View(result);
+        }
+
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            await _newsletterService.DeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
