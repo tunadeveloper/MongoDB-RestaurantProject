952c0db83c3dcc58fa4373a3d89bc758a8a77eed
tunadeveloper <tunabusiness25@gmail.com>
2026-02-08 21:01:12 +0300

    Updated

---
+++ MongoDB-RestaurantProject/Controllers/ChefController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB_RestaurantProject.DataTransferObject.ChefDTOs;
+using MongoDB_RestaurantProject.Services.ChefService;
+using System.Threading.Tasks;
+
+namespace MongoDB_RestaurantProject.Controllers
+{
+    public class ChefController : Controller
+    {
+        private readonly IChefService _chefService;
+        private readonly IMapper _mapper;
+        public ChefController(IChefService chefService, IMapper mapper)
+        {
+            _chefService = chefService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var list = await _chefService.GetListAsync();
+            var result = _mapper.Map<List<ResultChefDTO>>(list);
+            return View(result);
+        }
+    }
+}
