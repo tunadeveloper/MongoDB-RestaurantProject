f3a827989643e97b1a91a87c693df5aa44f01230
tunadeveloper <tunabusiness25@gmail.com>
2026-02-09 19:37:43 +0300

    Updated

--- MongoDB-RestaurantProject/Controllers/BlogController.cs
+++ MongoDB-RestaurantProject/Controllers/BlogController.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB_RestaurantProject.Context.Entities;
+using MongoDB_RestaurantProject.DataTransferObject.BlogCommentDTOs;
 using MongoDB_RestaurantProject.DataTransferObject.BlogDTOs;
+using MongoDB_RestaurantProject.Services.BlogCommentService;
 using MongoDB_RestaurantProject.Services.BlogService;
 using System.Threading.Tasks;
 
@@ -9,11 +12,13 @@ namespace MongoDB_RestaurantProject.Controllers
     public class BlogController : Controller
     {
         private readonly IBlogService _blogService;
+        private readonly IBlogCommentService _blogCommentService;
         private readonly IMapper _mapper;
-        public BlogController(IBlogService blogService, IMapper mapper)
+        public BlogController(IBlogService blogService, IMapper mapper, IBlogCommentService blogCommentService)
         {
             _blogService = blogService;
             _mapper = mapper;
+            _blogCommentService = blogCommentService;
         }
 
         public async Task<IActionResult> Index()
@@ -27,7 +32,20 @@ namespace MongoDB_RestaurantProject.Controllers
         {
             var list = await _blogService.GetByIdAsync(id);
             var result = _mapper.Map<ResultBlogDTO>(list);
+
+            var comments = await _blogCommentService.GetCommentsByBlogIdAsync(id);
+            var commentResult = _mapper.Map<List<ResultBlogCommentDTO>>(comments);
+            result.Comments = commentResult;
             return View(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateComment(CreateBlogCommentDTO createBlogCommentDTO)
+        {
+            createBlogCommentDTO.CreatedAt = DateTime.Now;
+            var entity = _mapper.Map<BlogComment>(createBlogCommentDTO);
+            await _blogCommentService.CreateAsync(entity);
+            return RedirectToAction("BlogDetails", new {id = createBlogCommentDTO.BlogId});
         }
     }
 }
---
+++ MongoDB-RestaurantProject/ViewComponents/Blog/_CreateBlogCommentComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB_RestaurantProject.DataTransferObject.BlogCommentDTOs;
+
+namespace MongoDB_RestaurantProject.ViewComponents.Blog
+{
+    public class _CreateBlogCommentComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke(CreateBlogCommentDTO dTO)
+        {
+            return View(dTO);
+        }
+    }
+}
